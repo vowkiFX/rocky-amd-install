46db59c5cfe5d0ccb55ab8745bb1c0dc81cb22f9
vowkiFX <bdamkvist@gmail.com>
2025-05-19 16:13:47 +0200

    Update 2.preinstall-amd.sh

--- 2.preinstall-amd.sh
+++ 2.preinstall-amd.sh
@@ -7,6 +7,11 @@ sudo crb enable
 
 # Perform a dnf update now
 sudo dnf update -y
+sudo dnf groupinstall "Development Tools" -y
+sudo dnf install kernel-devel gcc make -y
+sudo dnf install epel-release -y
+sudo dnf install dkms -y
+
 
 echo "$APP_NAME has been installed. Press Enter to reboot the system..."
 read
